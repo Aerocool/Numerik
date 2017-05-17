434d64687c0a9204c4b800cba1ba3415418942aa
Thilo Nietzschmann <thilo@nietzschmann.de>
2017-05-17 15:29:54 +0200

    an a7 gearbeitet

--- numerik/aufgabe7.py
+++ numerik/aufgabe7.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from time import *
 
 
 a = 9.8606
@@ -27,11 +28,21 @@ def sekantenVerfahren(function, x_k, x_kminus1, n):
     if n == 0:
         return x_k
     tmp = x_k
-    x_k = (x_k - (x_k - x_kminus1)/(function(x_k) - function(x_kminus1))) * function(x_k)
+    nenner = (function(x_k) - function(x_kminus1))
+    if nenner == 0: # Sobald das Sekantenverfahren nah an der NST ist, steht im Nenner eine Null, da der floating point nicht mehr genau genug ist
+        return x_k
+    x_k = (x_k - (x_k - x_kminus1)/nenner * function(x_k))
     x_kminus1 = tmp
     return sekantenVerfahren(function, x_k, x_kminus1, n-1)
 
 
+# Aufgabe 1
 function = function()
-print newtonVerfahren(function.b, function.bAbgeleitet, 1961, 15)
-print sekantenVerfahren(function.b, 2000, 1961, 15)
+timestamp = clock()
+print newtonVerfahren(function.b, function.bAbgeleitet, 1961, 5)
+print "Benoetigte Zeit (Newton): ", clock() - timestamp
+timestamp = clock()
+print sekantenVerfahren(function.b, 2000, 1961, 7)
+print "Benoetigte Zeit (Sekanten): ", clock() - timestamp
+# Der Zeitmessung nach bracht das Newtonverfahren minimal mehr Zeit um Maschinengenauigkeit zu erreichen,
+# jedoch weniger Iterationsschritte
